4edc6267e7bec661939657ef9f6e5f317fc232ad
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-02-01 14:18:00 +0100

    add functional registration form

--- js/registerform.js
+++ js/registerform.js
@@ -1,4 +1,5 @@
 // Form validation
+
 const registrationForm = document.getElementById("registration-form");
 
 const regUserName = document.getElementById("registration-user-name");
@@ -13,9 +14,41 @@ const regPasswordError = document.getElementById("registration-password-error");
 const regPasswordConfirm = document.getElementById("registration-user-password-confirm");
 const regPasswordConfirmError = document.getElementById("registration-password-confirm-error");
 
+
+
+// POST to API
+
+async function registerUser(url, data) {
+  try {
+    const postData = {
+      method: "post",
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(data),
+    };
+
+    const response = await fetch(url, postData);
+    console.log("response is", response)
+    const json = await response.json();
+    console.log("json", json);
+    return json;
+  } catch (error) {
+    console.log("error is", error);
+  }
+}
+
 function validateForm(event) {
   event.preventDefault();
+
+  const user = {
+    name: regUserName.value,
+    email: regEmail.value,
+    password: regPassword.value
+  };
   
+
+
 // Validating username
 
 if (checkRegUsername(regUserName.value)) {
@@ -59,7 +92,6 @@ function checkLength(value, len) {
 
 // Function checking email address
 
-
 function validateEmail(regEmail) {
   const regEx = /^[a-zA-Z0-9._%+-]+@(noroff\.no|stud\.noroff\.no)$/;
   const patternMatches = regEx.test(regEmail);
@@ -79,7 +111,21 @@ function checkPasswordConfirm(password, confirmPassword) {
   return password === confirmPassword;
 }
 
+const apiBaseUrl = "https://api.noroff.dev";
+
+registerUser(`${apiBaseUrl}/api/v1/social/auth/register`, user);
 }
 
-registrationForm.addEventListener("submit", validateForm);
+if (registrationForm) {
+  registrationForm.addEventListener("submit", validateForm);
+} else {
+  console.error("registration is incomplete");
+}
+
+
+
+
+
+
+
 

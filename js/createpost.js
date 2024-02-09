00f3659269b04a56327e1cd1424b31a9e8688713
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-02-09 14:46:35 +0100

    add posting to the API

--- js/createpost.js
+++ js/createpost.js
@@ -1,12 +1,21 @@
 import { apiBaseUrl, postsEndpoint } from "./api/constants.js"; 
 
 const postMessageForm = document.getElementById("post-message-form");
-
 const messageTitle = document.getElementById("messageTitle");
-
 const messageContent = document.getElementById("messageContent");
 
+function validatePostForm() {
+  if (!messageTitle.value.trim() || !messageContent.value.trim()) {
+    alert("Both title and content are required. This will be changed lager");
+    return false;
+  } 
+  return true
+  }
+
 async function createPost(url, message) {
+  if (!validatePostForm()) {
+    return;
+  }
   try {
 
     const token = localStorage.getItem("accessToken");
@@ -30,18 +39,22 @@ async function createPost(url, message) {
 
     console.log("json", json);
 
+    location.reload(true);
+
     return json;
   } catch (error) {
     console.log("error is", error);
   }
-
-  const postRequest = {
-    title: messageTitle.value,
-    body: messageContent.value
-  };
-
-  createPost(apiBaseUrl + postsEndpoint, postRequest)
-
 }
 
-postMessageForm.addEventListener("click", createPost);
+  postMessageForm.addEventListener("submit", async (event) => {
+  event.preventDefault();
+  if (validatePostForm()) {
+    const postRequest = {
+      title: messageTitle.value,
+      body: messageContent.value
+    };
+  
+  createPost(apiBaseUrl + postsEndpoint, postRequest);
+}
+  });

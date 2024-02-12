32d13fa1d0ab54e1eb6be4570d5b95db128aac4f
Tor-Haakon Bergseng <119207834+teehbee@users.noreply.github.com>
2024-02-12 10:55:34 +0100

    add sort on feed, created time on posts and change image in posts

--- js/posts.js
+++ js/posts.js
@@ -2,29 +2,33 @@ import { apiBaseUrl, postsEndpoint } from "./api/constants.js";
 
 const postContainerFeed = document.querySelector(".feed-container");
 const loadMoreLink = document.querySelector(".load-more");
+const sortOrderSelect = document.querySelector("#sortOrderSelect");
 
 let currentIndex = 10;
 
-async function fetchPosts(url) {
+async function fetchPosts(url, sortOrder = "desc") {
   try {
-    const token = localStorage.getItem(`accessToken`);
+    const token = localStorage.getItem("accessToken");
     const getData = {
-      method: `GET`,
+      method: "GET",
       headers: {
-        'Content-Type': 'application/json',
+        'Content-Type': "application/json",
         Authorization: `Bearer ${token}`,
       },
     };
-    const response = await fetch(url, getData);
+
+    const urlWithSortOrder = `${url}?sortOrder=${sortOrder}`;
+
+    const response = await fetch(urlWithSortOrder, getData);
     console.log(response);
     const posts = await response.json();
 
-
     console.log(posts);
+
+    postContainerFeed.innerHTML = "";
 
     posts.slice(0, currentIndex).forEach(post =>{      
     
-
     postContainerFeed.innerHTML += 
     `
   <div class="col-10 col-lg-5 bg-light m-lg-3 mt-4 min-height-150-px">
@@ -32,13 +36,14 @@ async function fetchPosts(url) {
           <div class="col-3 pt-4">
             <a href="../profile/index.html">
               <picture>
-                <source media="(min-width: 992px)" srcset="../assets/img/profile-post-large.png">
-                <img src="../assets/img/profile-post-small.png" alt="Logo of original poster">
+                <source media="(min-width: 992px)" srcset="../assets/logo/profile-image-large.png">
+                <img src="../assets/logo/profile-image-small.png" alt="Logo of original poster">
                 </picture></a>
+                <p class="post-body text-green-lg-white font-fm-mulish fs-0-625rem-lg-0-875rem pt-3">Post created: ${new Date(post.created).toLocaleDateString()}</p>
           </div>
-          <div class="post-title-and-text-container col-9">
-            <h5 class="post-title text-green-lg-white font-fm-mulish fs-6  fs-0-75rem-lg-1rem fw-bold pt-3 m-0 ">${post.title}</h5>
-            <p class="post-body text-green-lg-white font-fm-mulish fs-0-625rem-lg-0-875rem pt-1">${post.body}</p>
+          <div class="post-title-and-text-container col-9 ">
+            <h5 class="post-title text-green-lg-white font-fm-mulish fs-6 fs-0-75rem-lg-1rem fw-bold pt-4 m-0">${post.title}</h5>
+            <p class="post-body text-green-lg-white font-fm-mulish fs-0-75rem-lg-1rem pt-1">${post.body}</p>    
           </div>
         </div>
       </div>
@@ -54,12 +59,17 @@ async function fetchPosts(url) {
   }
 }
 
+fetchPosts(apiBaseUrl + postsEndpoint, "desc");
+
 loadMoreLink.addEventListener("click", (event) => {
   event.preventDefault();
   currentIndex += 10;
-  fetchPosts(apiBaseUrl + postsEndpoint);
+  fetchPosts(apiBaseUrl + postsEndpoint, "desc");
 });
 
-fetchPosts(apiBaseUrl + postsEndpoint);
+sortOrderSelect.addEventListener("change", (event) => {
+  const selectedSortOrder = event.target.value;
+  fetchPosts(apiBaseUrl + postsEndpoint, selectedSortOrder);
+})
 
 

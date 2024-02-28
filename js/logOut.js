44f9d1c05f6b7ebb20ffd5c725523e7ec191d785
teehbee <thbergseng@gmail.com>
2024-02-28 13:01:12 +0100

    logut functionality

--- js/logOut.js
+++ js/logOut.js
@@ -1,13 +1,13 @@
 const logOutIcon = document.getElementById("logOut");
 
-logOutIcon.addEventListener("click", function() {
-
+logOutIcon.addEventListener("click", function(event) {
+event.preventDefault();
   console.log('Logout clicked');
 
   localStorage.removeItem("registerName");
   localStorage.removeItem("userName");
   localStorage.removeItem("accessToken");
 
-  window.location.replace("../register.html");
+  window.location.href="../register.html";
 
 });
